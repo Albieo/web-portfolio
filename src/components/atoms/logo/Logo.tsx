1cb79d916bc3575f10d0cd7ecdd0910b1060be33
Albert Mpepo <albertmpepo@gmail.com>
2025-07-08 11:37:17 +0200

    Refrac - new logo added

--- src/components/atoms/logo/Logo.tsx
+++ src/components/atoms/logo/Logo.tsx
@@ -1,13 +1,13 @@
 import { ReactElement } from 'react'
 import Image from '../image/Image'
-import logo from '../../../img/devjane.png'
+import logo from '../../../img/AMlogo(clean).svg';
 import './logo.scss'
 
 const Logo = (): ReactElement => {
-  return (
-    <div className="logo">
-      <Image className='logo--img' src={logo} alt="web logo"/>
-    </div>
-  )
+    return (
+        <div className="logo">
+            <Image className='logo--img' src={logo} alt="web logo" />
+        </div>
+    )
 }
 export default Logo
