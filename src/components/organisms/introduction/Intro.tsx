c25a25963dd69a4b7435edd3979ec3d770b000fd
Albert Mpepo <albertmpepo@gmail.com>
2025-11-23 14:12:51 +0200

    Feat: add cursor animation

--- src/components/organisms/introduction/Intro.tsx
+++ src/components/organisms/introduction/Intro.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import Image from '../../atoms/image/Image'
+import SplashCursor from '../../atoms/splash-cursor/SplashCursor'
 import IntroContent from '../../molecules/intro-content/IntroContent'
 import img_intro from '../../../img/PortImage.jpg'
 import { useTheme } from '../../atoms/theme/ThemeContext';
@@ -12,6 +13,7 @@ const Intro = (): ReactNode => {
     <section className={`intro ${theme === 'light' ? 'intro--light' : 'intro--dark'}`} id="home">
       <IntroContent name={'Albert Mpepo'} subtitle="Full Stack Engineer" />
       <Image className="intro__img" src={img_intro} alt="a picture of Jane Smith smiling" />
+      <SplashCursor />
     </section>
   )
 }
