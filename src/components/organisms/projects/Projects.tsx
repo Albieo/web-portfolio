3c8393a2c5bc1eab200932e63efba6a8405067f2
Albert Mpepo <albertmpepo@gmail.com>
2025-11-23 14:49:30 +0200

    Feat: add project

--- src/components/organisms/projects/Projects.tsx
+++ src/components/organisms/projects/Projects.tsx
@@ -3,13 +3,13 @@ import SectionHeader from "../../molecules/section-header/SectionHeader";
 import PortfolioItem from "../../molecules/portfolio-item/PortfolioItem";
 import portfolio_01 from '../../../img/portfolio-01.jpg';
 import portfolio_02 from '../../../img/portfolio-02.jpg';
-import portfolio_03 from '../../../img/portfolio-03.jpg';
+import portfolio_03 from '../../../img/bash.png';
 import portfolio_04 from '../../../img/portfolio-04.jpg';
 import portfolio_05 from '../../../img/portfolio-05.jpg';
-import portfolio_06 from '../../../img/portfolio-06.jpg';
-import portfolio_07 from '../../../img/portfolio-07.jpg';
-import portfolio_08 from '../../../img/portfolio-08.jpg';
-import portfolio_09 from '../../../img/portfolio-09.jpg';
+// import portfolio_06 from '../../../img/portfolio-06.jpg';
+// import portfolio_07 from '../../../img/portfolio-07.jpg';
+// import portfolio_08 from '../../../img/portfolio-08.jpg';
+// import portfolio_09 from '../../../img/portfolio-09.jpg';
 // import portfolio_10 from '../../../img/portfolio-10.jpg';
 import './projects.scss';
 
@@ -18,11 +18,11 @@ const portfolioItems = [
   { src: portfolio_02, title: "AirBnB Clone", url: "https://github.com/Albieo/AirBnB_clone_v4" },
   { src: portfolio_03, title: "Number Guessing Game", url: "https://github.com/Albieo/Number-Guessing-Game" },
   { src: portfolio_04, title: "Basketball Scoreboard", url: "https://basketball-scoreboard-eta.vercel.app/" },
-  { src: portfolio_05, title: "Portfolio item 05" },
-  { src: portfolio_06, title: "Portfolio item 06" },
-  { src: portfolio_07, title: "Portfolio item 07" },
-  { src: portfolio_08, title: "Portfolio item 08" },
-  { src: portfolio_09, title: "Portfolio item 09" },
+  { src: portfolio_05, title: "StaffQuest", uri: "https://staffquest.onrender.com/" },
+  // { src: portfolio_06, title: "Portfolio item 06" },
+  // { src: portfolio_07, title: "Portfolio item 07" },
+  // { src: portfolio_08, title: "Portfolio item 08" },
+  // { src: portfolio_09, title: "Portfolio item 09" },
   // { src: portfolio_10, title: "Portfolio item 10" },
 ];
 
